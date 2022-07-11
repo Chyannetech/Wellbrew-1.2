dc8d501570715c04c836a15a52b8b408ce7adea6
Chyanne Robbins <chyannetech@gmail.com>
2022-07-11 18:11:37 -0400

    Add Business Model

--- main_app/views.py
+++ main_app/views.py
@@ -1,19 +1,20 @@
 
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Business
+# from django.http import HttpResponse
 
-class Business:
-    def __init__(self, name, address, city, state, phone, website):
-        self.name = name
-        self.address = address
-        self.city = city
-        self.state = state
-        self.phone = phone
-        self.website = website
+# class Business:
+#     def __init__(self, name, address, city, state, phone, website):
+#         self.name = name
+#         self.address = address
+#         self.city = city
+#         self.state = state
+#         self.phone = phone
+#         self.website = website
 
-businesses = [
-    Business('Yellow Green', '3080 Sheridan St', 'Hollywood', 'FL', '(954) 513-3990', 'https://www.ygfarmersmarket.com/')
-]
+# businesses = [
+#     Business('Yellow Green', '3080 Sheridan St', 'Hollywood', 'FL', '(954) 513-3990', 'https://www.ygfarmersmarket.com/')
+# ]
 
 # Create your views here.
 
@@ -25,6 +26,7 @@ def about(request):
     return render(request, 'about.html')
 
 def businesses_index(request):
+    businesses = Business.objects.all()
     return render(request, 'businesses/index.html', { 'businesses': businesses })
 
 
