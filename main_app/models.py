9b84147b204d9df7466dd757e01159b77c44f776
Chyanne Robbins <chyannetech@gmail.com>
2022-07-11 18:44:23 -0400

    Update Business Model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,4 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
@@ -6,6 +7,8 @@ class Business(models.Model):
     address = models.CharField(max_length=100)
     city = models.CharField(max_length=100)
     state = models.CharField(max_length=10)
-    phone = models.IntegerField()
+    phone = models.CharField(max_length=20)
     website = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
