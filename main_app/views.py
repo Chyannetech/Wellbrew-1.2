4103e5958646b83453854d7d5a2bc514cc9a40d0
Chyanne Robbins <chyannetech@gmail.com>
2022-07-11 03:47:45 -0400

    Create Business List

--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,29 @@
+
 from django.shortcuts import render
 from django.http import HttpResponse
 
+class Business:
+    def __init__(self, name, address, city, state, phone, website):
+        self.name = name
+        self.address = address
+        self.city = city
+        self.state = state
+        self.phone = phone
+        self.website = website
+
+businesses = [
+    Business('Yellow Green', '3080 Sheridan St', 'Hollywood', 'FL', '(954) 513-3990', 'https://www.ygfarmersmarket.com/')
+]
+
 # Create your views here.
 
-# Define the home view
 def home(request):
     return HttpResponse('<h1>Hello</h1>')
 
 def about(request):
     return render(request, 'about.html')
+
+def businesses_index(request):
+    return render(request, 'businesses/index.html', { 'businesses': businesses })
+
+
