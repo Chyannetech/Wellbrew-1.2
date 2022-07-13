b8fa4790059b113da7956201fff9126a7d0d00e2
Chyanne Robbins <chyannetech@gmail.com>
2022-07-13 14:57:55 -0400

    Add Authorization

--- main_app/models.py
+++ main_app/models.py
@@ -1,15 +1,7 @@
 
 from django.db import models
 from django.urls import reverse
-from django.contrib.auth.models import User 
-
-
-# Create your models here.
-# class Categories(models.Model):
-#     name:
-
-
-
+from django.contrib.auth.models import User
 
 class Business(models.Model):
     name = models.CharField(max_length=100)
--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,9 @@
-
-
-from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.shortcuts import render, redirect
 from .models import Business
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -12,24 +11,44 @@ def home(request):
     # return HttpResponse('<h1>Hello</h1>')
     return render(request, 'home.html')
 
-
 def about(request):
     return render(request, 'about.html')
 
-
+@login_required
 def businesses_index(request):
-    businesses = Business.objects.all()
+    businesses = Business.objects.filter(user=request.user)
     return render(request, 'businesses/index.html', {'businesses': businesses})
 
+@login_required
 def businesses_detail(request, business_id):
     business = Business.objects.get(id=business_id)
     return render(request, 'businesses/detail.html', { 'business' : business })
 
 
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      # This will add the user to the database
+      user = form.save()
+      login(request, user)
+      return redirect('index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  # A bad POST or a GET request, so render signup.html with an empty form
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
+
 class BusinessCreate(CreateView):
     model = Business
     fields = '__all__'
     success_url = '/businesses/'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
 
 class BusinessUpdate(UpdateView):
     model = Business
