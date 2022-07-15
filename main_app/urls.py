3ef2b1d5eca62037f7233286d571fff753ae8c17
Chyanne Robbins <chyannetech@gmail.com>
2022-07-15 10:51:41 -0400

    Update Route and CSS

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,7 +5,7 @@ from . import views
 # urlpatterns to hold each route
 urlpatterns = [
     path('', views.home, name='home'),
-    path('about/', views.about, name='about'),
+    path('directory/', views.directory, name='directory'),
     path('businesses/', views.businesses_index, name='index'),
     path('businesses/<int:business_id>/', views.businesses_detail, name='detail'),
     path('businesses/create', views.BusinessCreate.as_view(), name='businesses_create'),
--- main_app/views.py
+++ main_app/views.py
@@ -12,8 +12,8 @@ def home(request):
     # return HttpResponse('<h1>Hello</h1>')
     return render(request, 'home.html')
 
-def about(request):
-    return render(request, 'about.html')
+def directory(request):
+    return render(request, 'directory.html')
 
 @login_required
 def businesses_index(request):
